efb6f7a93a9ba05901f2823673209964637eb72c
Gregory Saumier-Finch <gregory@culturecreates.com>
2023-11-23 18:03:06 -0600

    added PostalAddress

--- src/seo.rb
+++ src/seo.rb
@@ -2,6 +2,9 @@
 
 require_relative './lib/pipeline.rb'
 
+# Related to "Add recommended properties to event JSON-LD in Open API"
+# https://github.com/culturecreates/footlight-calendar-api/issues/767
+
 pipeline = Pipeline.new( 
   frame: "./frame/frame-event-seo.jsonld",
   fixture: "./fixtures/alicia-moffet-event.json",
